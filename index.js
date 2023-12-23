c091ca1ea473c5f485b17549ea8f2250ff3ca13e
Pablo Alvarado <paalvarador@gmail.com>
2023-12-23 01:44:18 -0500

    Add some operations

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const operations = require("./operations");
+
 const greetings = (name, email) => {
   return `Welcome ${name}, your email is ${email}`;
 };
@@ -5,3 +7,9 @@ const greetings = (name, email) => {
 const greeting = greetings("Pablo Alvarado", "paalvarador@gmail.com");
 
 console.log(greeting);
+console.log(`la suma de 3 + 4 es igual a ${operations.sumar(3, 4)}`);
+console.log(`La reta de 3 - 4 es igual a ${operations.restar(3, 4)}`);
+console.log(
+  `la multiplica de 3 * 4 es igual a ${operations.multiplicar(3, 4)}`
+);
+console.log(`La division de 3 / 4 es igual a ${operations.dividir(3, 4)}`);
---
+++ operations.js
@@ -0,0 +1,15 @@
+export default function sumar(a, b) {
+  return a + b;
+}
+
+export default function restar(a, b){
+  return a - b;
+};
+
+export default function multiplicar(a, b){
+  return a * b;
+};
+
+export default function dividir(a, b){
+  return a / b;
+};
