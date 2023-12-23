22572b1d35d3a5e0c7475b7884c46a83dbe4d148
Pablo Alvarado <paalvarador@gmail.com>
2023-12-23 01:28:31 -0500

    Add index.js file

---
+++ index.js
@@ -0,0 +1,7 @@
+const greetings = (name, email) => {
+  return `Welcome ${name}, your email is ${email}`;
+};
+
+const greeting = greetings("Pablo Alvarado", "paalvarador@gmail.com");
+
+console.log(greeting);
